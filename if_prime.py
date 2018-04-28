3d68e406c0b6ac9fe35f79bc1805603f61247ff8
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-04-28 23:26:55 +0530

    Update if_prime.py

--- if_prime.py
+++ if_prime.py
@@ -1,20 +1,22 @@
 def ifprime(ip)  :
     output = 0
     ip1 = int(ip)
-    if ip1%2 == 0 and ip1 != 2 :
-       output = 'false'       
+    if ip1%2 == 0 and ip1 != 2 or ip1 == 1 :
+       output = 'false'
+    if ip1 == 2 or ip1 == 3 :
+        output = 'true'
     
     else :
-        checker = 0
+        checker = 1
         import math
         ip2 = math.sqrt(ip1)
         ip3 = math.ceil(ip2) + 1
-        print ('ip3', ip3)
+        #print ('ip3', ip3)
         for x in range(1,ip3) :
-            print ('x', x)
+            #print ('x', x)
             z = ip1%x
             if z == 0 :
-                print("Y")
+                #print("Y")
                 checker = checker + 1
                 
         if checker == 2 :
@@ -24,8 +26,8 @@ def ifprime(ip)  :
     print (output)
 
 
-ifprime(9)
+#ifprime(9)
 
-#for x in range(10):
+for x in range(10):
     #print(x)
-    #ifprime(x)
+    ifprime(x)
