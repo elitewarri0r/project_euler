ba7a511ecdc3808c33ed76e04ca3ff48ade7b6d4
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-04-28 12:27:18 +0530

    Update Problem 4.py

--- Problem 4.py
+++ Problem 4.py
@@ -1,1 +1,18 @@
 
+#problem 4
+# built-in function for flipping str, x = x[::-1]
+
+multiplier1 = 100
+multiplier2 = 100
+op = 0
+product1 = multiplier1*multiplier2
+
+for x in range(900) :
+    product1 = multiplier1*multiplier2
+    y = str(product1)
+    #print (y)
+    z = (y[::-1])
+    if z == y :
+        op = product1
+    multiplier1 = multiplier1 + 1
+    multiplier2 = multiplier2 + 1
