aa994351353bc0667f25b4cb4edd6c9041193bd2
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-04-28 12:26:16 +0530

    Update Problem 3.py

--- Problem 3.py
+++ Problem 3.py
@@ -1,1 +1,31 @@
 
+# problem three
+
+ip = 12345
+ifprime = 0
+highestp = 2
+op = 0
+opC = 0
+while 0==0 :
+    opC = op
+    for x in range(1,(ip+1)) :
+        if ip%x == 0 :
+            ifprime = ifprime + 1
+    if ifprime == 2 :
+        print ('The input is prime')
+        ifprime = 0
+        break
+    else :
+        for x in range(0,ip) :
+            for z in range (1,x+1) :
+                if x%z == 0 :
+                    ifprime = ifprime + 1
+            if ifprime == 2 and x > 1 :
+                highestp = x
+                print (highestp)
+            if ip%highestp == 0 :
+                op = highestp
+    print (op)
+    if opC == op :
+        break 
+
