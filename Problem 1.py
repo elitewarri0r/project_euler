9f609965415316222f44a25b95866f183bee0ffb
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-04-28 12:24:34 +0530

    Update Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -1,1 +1,14 @@
+# Find the sum of all the multiples of 3 or 5 below 1000.
+
+print ('problem 1')
+input2 = input('Number to not exceed : ')
+output = 0
+input1 = int(input2)
+for x in range (1,input1) :
+    three = x%3
+    five = x%5
+    if five == 0 or three == 0 :
+        output = output + x
+
+print (output)
 
