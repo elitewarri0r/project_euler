80ea0c4bb3fba7e808b1a159a1214c3014cd7dbb
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-05-03 20:49:36 +0530

    Create Problem 6.py

---
+++ Problem 6.py
@@ -0,0 +1,13 @@
+totalsq = 0
+eachsq = 0
+
+for x in range(1,101) :
+    totalsq = totalsq + x
+    eachsq = eachsq + (x*x)
+
+totalsq = totalsq * totalsq
+print(totalsq)
+print(eachsq)
+output = totalsq - eachsq
+
+print ('The final output is ',output)
