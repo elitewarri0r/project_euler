1a2d1f3ae6fb77c7acdbca3fe9879b9420043e03
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-05-03 21:02:56 +0530

    Update Problem 6.py

--- Problem 6.py
+++ Problem 6.py
@@ -1,3 +1,17 @@
+
+'''
+The sum of the squares of the first ten natural numbers is,
+1x1 + 2x2 + ... + 10x10 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)x(1 + 2 + ... + 10) = 55x55 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+
+'''
 totalsq = 0
 eachsq = 0
 
