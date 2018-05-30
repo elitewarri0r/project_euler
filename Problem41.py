d6e9be623c8731d94adb30f2a83c49c655b3ef7b
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-05-30 22:23:11 +0530

    Create Problem41.py

---
+++ Problem41.py
@@ -0,0 +1,32 @@
+import math
+def isPrime(ip)  :
+    output = 1
+    ip1 = int(ip)
+    
+    if ip1 == 2 or ip1 == 3 :
+        output = 1
+    elif ip1 % 2 == 0 or ip1 == 1 :
+       output = 0
+    
+    else :
+        ip2 = math.sqrt(ip1)
+        ip3 = math.ceil(ip2) + 1
+        #print ('ip3', ip3)
+        for x in range(3,ip3) :
+            if ip1 % x == 0 :
+                output = 0
+    return output
+
+number = 50 
+for x in range(1000000000) :
+    print(number)
+    if isPrime(number) == 1 :
+        num = sorted(list(str(number)))
+        l = len(num) 
+        for x in range(l) :
+            if num[x] is x+1 :
+                if x == l :
+                    print(number)
+            else :
+                break
+    number = number + 1
