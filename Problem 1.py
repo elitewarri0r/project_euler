0e1cbaa278ad3a86cc56ec995426509cf13e83ad
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-05-30 19:18:13 +0530

    Update Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -1,14 +1,21 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
 print ('problem 1')
-input2 = 1000
 output = 0
-input1 = int(input2)
-for x in range (1,input1+1) :
+# Giving a variable named output the value 0
+input1 = 1001
+# The limit for the time the for loop should run
+for x in range (1,input1) :
+# A for loop that runs 1000 times
     three = x%3
+    # Giving a variable named three and giving it the reminder of x divided by 3
     five = x%5
+    # Giving a variable named three and giving it the reminder of x divided by 5
     if five == 0 or three == 0 :
+    #  The if condition that checks if x is a multiple of 3 or 5   
         output = output + x
+        # And if the above condition is true it adds x to the output
 
 print (output)
+# Printing the output
 
