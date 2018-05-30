3f61860ccd3823d8d8172c2a37b5cac4cc95d8fd
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-05-30 19:57:29 +0530

    Update Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -2,15 +2,15 @@
 
 print ('problem 1')
 output = 0
-# Giving a variable named output the value 0
+# Assigning a variable named output the value 0
 input1 = 1001
-# The limit for the time the for loop should run
+# The limit for the number time the for loop should run
 for x in range (1,input1) :
 # A for loop that runs 1000 times
     three = x%3
-    # Giving a variable named three and giving it the reminder of x divided by 3
+    # Assigning a variable named three and giving it the reminder of x divided by 3
     five = x%5
-    # Giving a variable named three and giving it the reminder of x divided by 5
+    # Assigning a variable named three and giving it the reminder of x divided by 5
     if five == 0 or three == 0 :
     #  The if condition that checks if x is a multiple of 3 or 5   
         output = output + x
