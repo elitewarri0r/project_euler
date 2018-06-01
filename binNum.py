34de7d61ab5e6d4dbe3d9c735b1b926e78f7c8a2
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-06-01 18:58:21 +0530

    Added the commets

--- binNum.py
+++ binNum.py
@@ -4,14 +4,13 @@ This function can convert any number into binary
 
 import math
 def binNum(num):
+     num = num*2
     quotient = 0
-    quotient = math.floor(num/2)
-    if num % 2 == 0 :
-        base2 = '0' 
-    else :
-        base2 = '1' 
+
     while quotient !=1 :
+        # We divide the input by two 
         quotient = math.floor(num/2)
+        # And if the quotient is a even number we add '0' to the ouput or '1' if its odd
         if quotient % 2 == 0 :
             base2 = '0' + base2
         else :
