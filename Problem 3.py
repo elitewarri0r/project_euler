db66732cadb17c0484377f7d3fd2a7652e5b8723
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-06-01 17:49:02 +0530

    Added the commets

--- Problem 3.py
+++ Problem 3.py
@@ -3,46 +3,43 @@
 The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 '''
-# problem three
-# the isPrime function
+
 import math
 def isPrime(ip)  :
     output = 1
     ip1 = int(ip)
     
+    # if input is 2 or 3 the output will become positive
     if ip1 == 2 or ip1 == 3 :
         output = 1
+        
+    # if input is a even-valued term or 1 the output will become negative 
     elif ip1 % 2 == 0 or ip1 == 1 :
        output = 0
     
     else :
         ip2 = math.sqrt(ip1)
         ip3 = math.ceil(ip2) + 1
-        #print ('ip3', ip3)
+        
+    # When this loop runs we will divide the number to see if it has any factors  
     for x in range(3,ip3) :
-            #print ("its x",x)
             if ip1 % x == 0 :
-                #print ("Y")
                 output = 0
     return output
-# end of the isPrime funtion
 
-inp = input('no. : ')
-inp251 = int(inp) #inp25 is numerical input
-#inp25 = math.sqrt(inp251)
-#inp3 = math.ceil(inp25)
+inp = int(input('enter a number : '))
+inp251 = int(inp) 
 inp3 = inp251/2
 inp3 = math.ceil(inp3)
-# print(inp3,inp251)
 
+# When this loop runs backwards we will find the prime that can divide the input
 for x in range(inp3, 0, -1) :
     isp = isPrime(x)
-    #print (isp,' and ',x)
+    
     if isp == 1 :
-        #print ('yea got in')
+        
         if inp251%x == 0 :
-            #print ('the isp is ',isp, 'and', x)
-            print('the highest prime factor of ',inp251,'is ',x)
+            print('The highest prime factor of ',inp251,'is ',x)
             break
   
 
