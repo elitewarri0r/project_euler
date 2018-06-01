558d9c7a9359a25bb5ed68c8c9ded21dda730edb
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-06-01 18:03:24 +0530

    Added the commets

--- Problem 5.py
+++ Problem 5.py
@@ -1,19 +1,29 @@
 
 '''
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+The sum of the squares of the first ten natural numbers is,
+12 + 22 + ... + 102 = 385
+
+The square of the sum of the first ten natural numbers is,
+(1 + 2 + ... + 10)2 = 552 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
 '''
 
 totalsq = 0
 eachsq = 0
 
-for x in range(1,101) :
-    totalsq = totalsq + x
+# When this loop runs we do both sum of the numbers and sum of the squares of the numbers
+for x in range(1,101) 
+    totalsq = totalsq + 
     eachsq = eachsq + (x*x)
 
+ # Here we square the sum of the numbers
 totalsq = totalsq * totalsq
 print(totalsq)
 print(eachsq)
 output = totalsq - eachsq
 
-print ('The final output is ',output)
+print(output,'is the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.')
