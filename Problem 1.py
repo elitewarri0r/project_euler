e137887054aecd724f265a4368e145d14006ee40
elitewarri0r <38175627+elitewarri0r@users.noreply.github.com>
2018-06-01 14:37:46 +0530

    Update Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -2,20 +2,17 @@
 
 print ('problem 1')
 output = 0
-# Assigning a variable named output with the value 0
 input1 = int(input('enter a number : '))
-# The limit for the number time the for loop should run
-for x in range (1,input1) :
-# A for loop that runs 1000 times
-    three = x%3
-    # Assigning a variable named three and giving it the reminder of x divided by 3
-    five = x%5
-    # Assigning a variable named three and giving it the reminder of x divided by 5
-    if five == 0 or three == 0 :
-    #  The if condition that checks if x is a multiple of 3 or 5   
-        output = output + x
-        # And if the above condition is true it adds x to the output
 
-print (output)
-# Printing the output
+# When this loop runs we pick all the multiples of 3 or 5
+for x in range (1,input1) :
+    three = x%3
+    five = x%5
+ 
+    if five == 0 or three == 0 :
+        # If x is a multiple of 3 or 5 we add it to the output
+         output = output + x
+
+print (output,'is the sum of all the multiples of 3 or 5 below 1000.')
+
 
